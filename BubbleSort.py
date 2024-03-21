a5032aacf33ab5c108ae0997506e1f43b365a0a4
ItzelMonce <itzelmonce6@gmail.com>
2024-03-20 23:04:41 -0600

    Programa que ordena una lista por el metodo de ordenamiento bubblesort

---
+++ BubbleSort.py
@@ -0,0 +1,20 @@
+def bsort(arreglo):
+    n=len(arreglo)
+
+#print(f'Tamaño de la lista: {n}')
+    for i in range(0,n-2):
+        for j in range(0, n-1):
+           if  arreglo[j]>arreglo[j+1]:
+            temp = arreglo[j]
+            arreglo[j]= arreglo[j+1]
+            arreglo[j+1]=temp
+     
+    return arreglo   
+
+listaDesordenada =[8,7,6,5,4,3,2,1]
+
+print(listaDesordenada)
+
+listaOrdenada=bsort(listaDesordenada)
+
+print(listaOrdenada)
